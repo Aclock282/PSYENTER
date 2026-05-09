9961d75da2bcac9ee429fe1c78c7a1404e0b65f8
Aclock282 <73348909+Aclock282@users.noreply.github.com>
2026-05-10 07:11:56 +0900

    Update DATABASEINFO.py

--- DATABASEINFO.py
+++ DATABASEINFO.py
@@ -1,5 +1,9 @@
+import os
+
+TOKEN = os.getenv("NOTION_TOKEN")
+
 class DB:
     def __init__(self):
-        self.TOKEN = "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"
+        self.TOKEN = TOKEN
         self.ID = "35b59828d4a3805f8629ebd6b7fc14ce"
         pass
