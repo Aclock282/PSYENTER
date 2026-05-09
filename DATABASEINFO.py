ac46f822d062a6e4637069165bb06e904dfb7046
Aclock282 <73348909+Aclock282@users.noreply.github.com>
2026-05-10 07:06:37 +0900

    Add files via upload

---
+++ DATABASEINFO.py
@@ -0,0 +1,5 @@
+class DB:
+    def __init__(self):
+        self.TOKEN = "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"
+        self.ID = "35b59828d4a3805f8629ebd6b7fc14ce"
+        pass
---
+++ MAIN.py
@@ -0,0 +1,50 @@
+import requests
+from DATABASEINFO import DB
+import math as m
+
+NDB = DB()
+
+headers = {
+    "Authorization": "Bearer " + NDB.TOKEN,
+    "Content-Type": "application/json",
+    "Notion-Version": "2026-03-11" # 최신 버전 확인 권장
+}
+
+def create_page(description, date, n1, n2): # status 기본값 설정
+    create_url = "https://api.notion.com/v1/pages"
+    
+    data = {
+        "parent": { "database_id": NDB.ID },
+        "properties": {
+            "이름": {
+                "title": [
+                    { "text": { "content": description } }
+                ]
+            },
+            "날짜": {
+                "date": { "start": date }
+            },
+            "숫자": {
+                "number": n1
+            },
+            "숫자1": {
+                "number": n2
+            },
+            "숫자2": {
+                "number": (m.sqrt(n1) + m.cos(n2))
+            }
+            
+        }
+    }
+    
+    response = requests.post(create_url, headers=headers, json=data)
+    
+    # 수정된 부분: .status_code 사용
+    if response.status_code == 200:
+        print(f"'{description}' 데이터 업로드 성공!")
+    else:
+        print(f"오류 발생: {response.status_code}")
+        print(response.text)
+
+# 실행 예시
+create_page("파이썬 공부하기", "2026-05-10", 3, m.pi/2)
